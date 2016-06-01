7dd9fe12bc23caf8f4ad1b5925907077fbd82446
Karol <187746@edu.p.lodz.pl>
2016-06-01 18:15:58 +0200

    refactor

--- Pam/RectFilter.cs
+++ Pam/RectFilter.cs
@@ -17,16 +17,19 @@ namespace Pam
             if (tooFar(rect))
                 Rectangle = rect;
             else
-            {
-                const int factNum = 3;
-                const int factDen = 5;
-                const int oneMinFactNum = factDen - factNum;
-                int left = (Rectangle.Left * factNum + rect.Left * oneMinFactNum) / factDen;
-                int rigth = (Rectangle.Right * factNum + rect.Right * oneMinFactNum) / factDen;
-                int top = (Rectangle.Top * factNum + rect.Top * oneMinFactNum) / factDen;
-                int bottom = (Rectangle.Bottom * factNum + rect.Bottom * oneMinFactNum) / factDen;
-                Rectangle = new Rectangle(left, top, rigth - left, bottom - top);
-            }
+                Rectangle = calcFiltered(rect);
+        }
+
+        private Rectangle calcFiltered(Rectangle rect)
+        {
+            const int factNum = 3;
+            const int factDen = 5;
+            const int oneMinFactNum = factDen - factNum;
+            int left = (Rectangle.Left * factNum + rect.Left * oneMinFactNum) / factDen;
+            int rigth = (Rectangle.Right * factNum + rect.Right * oneMinFactNum) / factDen;
+            int top = (Rectangle.Top * factNum + rect.Top * oneMinFactNum) / factDen;
+            int bottom = (Rectangle.Bottom * factNum + rect.Bottom * oneMinFactNum) / factDen;
+            return new Rectangle(left, top, rigth - left, bottom - top);
         }
 
         private bool tooFar(Rectangle rect)
