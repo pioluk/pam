c50d9636a5abc3e2d85802a3893be5936387ffdb
Karol <187746@edu.p.lodz.pl>
2016-06-01 18:08:23 +0200

    przepuszczanie duzych zmian.

--- Pam/RectFilter.cs
+++ Pam/RectFilter.cs
@@ -14,14 +14,32 @@ namespace Pam
 
         public void add(Rectangle rect)
         {
-            const int factNum = 3;
-            const int factDen = 5;
-            const int oneMinFactNum = factDen - factNum;
-            int left = (Rectangle.Left * factNum + rect.Left * oneMinFactNum) / factDen;
-            int rigth = (Rectangle.Right * factNum + rect.Right * oneMinFactNum) / factDen;
-            int top = (Rectangle.Top * factNum + rect.Top * oneMinFactNum) / factDen;
-            int bottom = (Rectangle.Bottom * factNum + rect.Bottom * oneMinFactNum) / factDen;
-            Rectangle = new Rectangle(left, top, rigth - left, bottom - top);
+            if (tooFar(rect))
+                Rectangle = rect;
+            else
+            {
+                const int factNum = 3;
+                const int factDen = 5;
+                const int oneMinFactNum = factDen - factNum;
+                int left = (Rectangle.Left * factNum + rect.Left * oneMinFactNum) / factDen;
+                int rigth = (Rectangle.Right * factNum + rect.Right * oneMinFactNum) / factDen;
+                int top = (Rectangle.Top * factNum + rect.Top * oneMinFactNum) / factDen;
+                int bottom = (Rectangle.Bottom * factNum + rect.Bottom * oneMinFactNum) / factDen;
+                Rectangle = new Rectangle(left, top, rigth - left, bottom - top);
+            }
+        }
+
+        private bool tooFar(Rectangle rect)
+        {
+            const int clNum = 1;
+            const int clDen = 10;
+            int mx_dx = Rectangle.Width * clNum / clDen;
+            int mx_dy = Rectangle.Height * clNum / clDen;
+            int dleft = Math.Abs(Rectangle.Left - rect.Left);
+            int drigth = Math.Abs(Rectangle.Right - rect.Right);
+            int dtop = Math.Abs(Rectangle.Top - rect.Top);
+            int dbottom = Math.Abs(Rectangle.Bottom - rect.Bottom);
+            return (dleft > mx_dx) || (drigth > mx_dx) || (dtop > mx_dy) || (dbottom > mx_dy);
         }
 
     }
