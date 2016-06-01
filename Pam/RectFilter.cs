6d48d89ce91ead62f8efcf32353adf31f9653633
Karol <187746@edu.p.lodz.pl>
2016-06-01 17:44:14 +0200

    prosty filtr drgan

--- Pam/RectFilter.cs
+++ Pam/RectFilter.cs
@@ -10,11 +10,19 @@ namespace Pam
     internal struct RectFilter
     {
 
+        private const int factNum = 3;
+        private const int factDen = 5;
+        private int oneMinFactNum { get { return factDen - factNum; } }
+
         public Rectangle Rectangle { get; private set; }
 
         public void add(Rectangle rect)
         {
-            Rectangle = rect;
+            int left = (Rectangle.Left * factNum + rect.Left * oneMinFactNum) / factDen;
+            int rigth = (Rectangle.Right * factNum + rect.Right * oneMinFactNum) / factDen;
+            int top = (Rectangle.Top * factNum + rect.Top * oneMinFactNum) / factDen;
+            int bottom = (Rectangle.Bottom * factNum + rect.Bottom * oneMinFactNum) / factDen;
+            Rectangle = new Rectangle(left, top, rigth - left, bottom - top);
         }
     }
 }
