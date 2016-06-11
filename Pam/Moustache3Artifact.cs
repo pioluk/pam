4ac8a86ff49b3b4f6bd88f1344186af2598f82dc
187716 <michal12594@gmail.com>
2016-06-11 20:38:02 +0200

    Dodanie kilku artefaktów

---
+++ Pam/Moustache3Artifact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Pam
+{
+    internal class Moustache3Artifact : IArtifact
+    {
+        private static readonly Bitmap artifact = new Bitmap("moustache3.png");
+
+        public void draw(Graphics g, Rectangle face)
+        {
+            float ratio = (float)face.Width / artifact.Width;
+            Size newSize = new Size((int)(artifact.Width * ratio * 1.6), (int)(artifact.Height * ratio * 1.6));
+            Point newPosition = new Point(face.X + (int)(face.Width * 0.97) - (int)(newSize.Width * 0.8), face.Y + (int)(face.Height * 0.9) - (int)(newSize.Height / 1.6));
+            g.DrawImage(artifact, new Rectangle(newPosition, newSize));
+        }
+    }
+}
