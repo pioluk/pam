acaef3232cac549fc68886317e2036979cbb5c11
Karol <187746@edu.p.lodz.pl>
2016-06-12 15:24:31 +0200

    Wrzucenie projektu PTst, w ktorym mozna sobie zobaczyc jak zmieni sie distanceFactor w zaleznosci od wzajemnnego polozenia dwoch prostokatow.

---
+++ PTst/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTst
+{
+    public partial class Form1 : Form
+    {
+
+        public Rectangle rect1 = new Rectangle(256, 128, 128, 128);
+        public Rectangle rect2 = new Rectangle(0, 0, 128, 128);
+        public Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            redraw();
+        }
+
+        public void redraw()
+        {
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                g.DrawRectangle(Pens.Blue, rect1);
+                g.DrawRectangle(Pens.Green, rect2);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Console.Out.WriteLine("X = {0}  Y = {1}", e.X, e.Y);
+            Point c = RectUtils.rectCenter(rect2);
+            rect2.Offset(e.X - c.X, e.Y - c.Y);
+            redraw();
+            Console.Out.WriteLine("F = {0}", RectUtils.distanceFactor(rect1, rect2));
+        }
+    }
+}
---
+++ PTst/RectUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+
+namespace PTst
+{
+    class RectUtils
+    {
+        public static double distanceFactor(Rectangle ro, Rectangle rn)
+        {
+            return (4 * rectsDistance(ro, rn) + squaredCentersDistance(ro, rn) + sizeChange(ro, rn)) / (double)((ro.Width + rn.Width) * (ro.Height + rn.Height));
+        }
+
+        public static ulong rectsDistance(Rectangle a, Rectangle b)
+        {
+            int dl = a.Left - b.Left;
+            int dr = a.Right - b.Right;
+            int dt = a.Top - b.Top;
+            int db = a.Bottom - b.Bottom;
+            ulong ll = (ulong)(dl * dl);
+            ulong rr = (ulong)(dr * dr);
+            ulong tt = (ulong)(dt * dt);
+            ulong bb = (ulong)(db * db);
+            return ll + rr + tt + bb;
+        }
+
+        public static ulong squaredCentersDistance(Rectangle a, Rectangle b)
+        {
+            return squaredDistance(rectCenter(a), rectCenter(b));
+        }
+
+        public static Point rectCenter(Rectangle rect)
+        {
+            return new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+        }
+
+        public static ulong sizeChange(Rectangle a, Rectangle b)
+        {
+            return squaredDistance(new Point(a.Size), new Point(b.Size));
+        }
+
+        public static ulong squaredDistance(Point a, Point b)
+        {
+            int dx = a.X - b.X;
+            int dy = a.Y - b.Y;
+            ulong xx = (ulong)(dx * dx);
+            ulong yy = (ulong)(dy * dy);
+            return xx + yy;
+        }
+
+    }
+}
