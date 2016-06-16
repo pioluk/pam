80b7b11ef65fdec83e9bb693b1c3b64be797266e
Karol <187746@edu.p.lodz.pl>
2016-06-16 22:15:12 +0200

    dodanie jednego śmigła wykonującego pół obrotu

--- Pam/Artifacts/Cap.cs
+++ Pam/Artifacts/Cap.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace Pam.Artifacts
 {
@@ -13,15 +14,46 @@ namespace Pam.Artifacts
 
         public void draw(Graphics g, Rectangle face)
         {
-            drawBase(g, face);
+            Rectangle drawRect = calcDrawRect(face);
+            g.DrawImage(capBase, drawRect);
+            drawAirscrew(g, drawRect);
         }
 
-        private void drawBase(Graphics g, Rectangle face)
+        private Rectangle calcDrawRect(Rectangle face)
         {
             float ratio = (float)face.Width / capBase.Width * 1.1f;
             Size newSize = new Size((int)(capBase.Width * ratio), (int)(capBase.Height * ratio));
             Point newPosition = new Point(face.X + (face.Width / 2) - (newSize.Width / 2), (int)(face.Y - newSize.Height * 0.7));
-            g.DrawImage(capBase, new Rectangle(newPosition, newSize));
+            return new Rectangle(newPosition, newSize);
         }
+
+        private void drawAirscrew(Graphics g, Rectangle drawRect)
+        {
+            int xc = drawRect.Width / 2;
+            int r1 = xc;
+            int r0 = r1 / 10;
+
+            float pos;
+            bool flip = calcPosition(out pos);
+            float apos = Math.Abs(pos);
+
+            int x1 = (int)((1.0f - apos) * -r0);
+            int x2 = (int)(r1 * apos);
+
+            g.FillEllipse(Brushes.Aqua, new Rectangle(drawRect.X + xc + x1, drawRect.Y + drawRect.Height / 10, x2 - x1, drawRect.Height / 10));
+        }
+
+        private bool calcPosition(out float pos)
+        {
+            const uint S = 2048;
+            uint time = GetTickCount() % S;
+            double phase = time * Math.PI * 2 / S;
+            pos = (float)Math.Cos(phase);
+            return time >= (S / 2);
+        }
+
+        [DllImport("Kernel32.dll", EntryPoint = "GetTickCount", ExactSpelling = true, SetLastError = false)]
+        private static extern uint GetTickCount();
+
     }
 }
